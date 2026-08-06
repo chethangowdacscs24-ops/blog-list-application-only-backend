f9c4a1646b78a04f800a701781c01ca39409fd7a
chethangowdacscs24-ops <chethangowdacs.cs24@bmsce.ac.in>
2026-08-06 22:48:15 +0530

    Refactor blog model and enhance error handling in blog routes

--- app.js
+++ app.js
@@ -16,5 +16,6 @@ mongoose.connect(config.MONGODB_URI,{family : 4}).then((result)=>{
 
 app.use('/api/blogs',blogRouter)
 app.use(Middleware.unknownEndpoint)
+app.use(Middleware.errorHandler)
 
 module.exports =app;
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,29 +1,32 @@
-const Blog = require('../models/blog')
-const blogRouter = require('express').Router()
-blogRouter.get('/',(request, response)=>{
-    Blog.find({}).then((result)=>{
+const Blog = require("../models/blog");
+const blogRouter = require("express").Router();
+blogRouter.get("/", (request, response) => {
+  Blog.find({})
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+blogRouter.get("/:id", (request, response) => {
+    Blog.find({_id: request.params.id})
+      .then((result) => {
         response.json(result);
-    }).catch((error)=>{
+      })
+      .catch((error) => {
         next(error);
+      });
+  });
+blogRouter.delete("/:id", (request, response) => {
+
+  Blog.findByIdAndDelete(request.params.id)
+    .then((savedblog) => {
+      response.status(202).end();
     })
-    
-})
-blogRouter.post('/',(request, response)=>{
-    const body = request.body
-    if(!body || !body.title){
-       return response.status(400).json({error : "title is missing"})
-    }
-    const blog = new Blog({
-        title: body.title ,
-        author: body.author ,
-        url : body.url ,
-        likes : body.likes ,
-    })
-    blog.save().then((savedblog)=>{
-        response.status(201).json(savedblog);
-    }).catch((error)=>{
-        next(error)
-    })
-    ;
-})
-module.exports = blogRouter
+    .catch((error) => {
+      next(error);
+    });
+});
+
+module.exports = blogRouter;
