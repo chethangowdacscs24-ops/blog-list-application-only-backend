7889880da4ccbc2259929ae4a46d5aa8b266db42
chethangowdacscs24-ops <chethangowdacs.cs24@bmsce.ac.in>
2026-08-07 15:33:15 +0530

    Add unit tests for blog functionality and implement helper functions for likes calculation

---
+++ utils/list_helper.js
@@ -0,0 +1,12 @@
+const dummy = (blog)=>{
+    return 1;
+}
+const totalLikes =(blogs)=>{
+    
+    const reducer =( sum , blog) =>{
+        
+        return sum + blog.likes ;
+    }
+   return blogs.reduce(reducer, 0);
+}
+module.exports ={dummy, totalLikes}
